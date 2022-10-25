606ea74ed4fb306bf5838228f78b73401e758bb4
martinezmarcos93 <115021940+martinezmarcos93@users.noreply.github.com>
2022-10-25 00:06:30 -0300

    "primer modelo"

--- NMR_Food/admin.py
+++ NMR_Food/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from NMR_Food.models import Cliente
 
-# Register your models here.
+admin.site.register(Cliente)
--- NMR_Food/models.py
+++ NMR_Food/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model) : 
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    mail = models.CharField(max_length=100)
+    direccion = models.IntegerField()
