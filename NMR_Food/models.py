81b2d287de49f495a2973d406149a2a91c56d3aa
Patricio Noce <patricionoce@gmail.com>
2022-10-25 00:32:22 -0300

    Modificacion def en class cliente

--- NMR_Food/models.py
+++ NMR_Food/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class Cliente(models.Model) : 
     nombre = models.CharField(max_length=100)
     telefono = models.CharField(max_length=100)
     mail = models.CharField(max_length=100)
-    direccion = models.CharField()
+    direccion = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"Nombre:{self.nombre}, Telefono:{self.telefono}, Mail:{self.mail}, Direccion:{self.direccion}"
+
